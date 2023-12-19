90d930d12e1a79ada6c8f4de82dbc48046bb5f80
loss23 <mebob248888@gmail.com>
2023-12-19 16:42:29 -0600

    FireBaseLogin

---
+++ js/FireBaseLogin.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithRedirect, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiXlz3jeuaVms1irD3rKlwVNE_DwnO14o",
+  authDomain: "pinguinstudiozweb.firebaseapp.com",
+  projectId: "pinguinstudiozweb",
+  storageBucket: "pinguinstudiozweb.appspot.com",
+  messagingSenderId: "197115887577",
+  appId: "1:197115887577:web:fa4c00a8bc48f63c6123c8"
+};
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+document.getElementById('SignUp').addEventListener("click", (e) => { 
+    const email = document.getElementById('Email').innerHTML;
+    const password = document.getElementById('Password').innerHTML;
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+});
+})
