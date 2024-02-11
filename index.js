fa2e623ea4cc81d6aaee9d5b9aa2f478207cd3ce
loss23 <mebob248888@gmail.com>
2024-02-10 21:14:57 -0600

    Added things

---
+++ index.js
@@ -0,0 +1,14 @@
+var EmpireZ_Link = document.getElementById("EmpireZ_Link");
+var Pinguin_Link = document.getElementById("Pinguin_Link");
+
+EmpireZ_Link.onclick = function() {
+    window.location.assign("https://empirez-2023-edition.pages.dev")
+};
+
+Pinguin_Link.onclick = function() {
+    window.location.assign("https://www.roblox.com/groups/13056582/Pinguin-Studioz#!/about")
+};
+
+Lunar_Link.onclick = function() {
+    window.location.assign("https://www.roblox.com/groups/33652903/Lunar-Works#!/about")
+};
