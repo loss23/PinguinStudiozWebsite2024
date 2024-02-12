a8891844d6e6853e47a699c59a4b8b02891a9314
loss23 <mebob248888@gmail.com>
2024-02-12 17:26:52 -0600

    S

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 var EmpireZ_Link = document.getElementById("EmpireZ_Link");
 var Pinguin_Link = document.getElementById("Pinguin_Link");
+var Lunar_Link = document.getElementById("Lunar_Link");
+
+var EmpireZ_Link2 = document.getElementById("EmpireZ_Link2");
+var Pinguin_Link2 = document.getElementById("Pinguin_Link2");
+var Lunar_Link2 = document.getElementById("Lunar_Link2");
 
 EmpireZ_Link.onclick = function() {
     window.location.assign("https://empirez-2023-edition.pages.dev")
 };
 
 Pinguin_Link.onclick = function() {
-    window.location.assign("https://www.roblox.com/groups/13056582/Pinguin-Studioz#!/about")
+    window.location.assign("PinguinStudioz.html")
 };
 
 Lunar_Link.onclick = function() {
+    window.location.assign("https://www.roblox.com/groups/33652903/Lunar-Works#!/about")
+};
+
+EmpireZ_Link2.onclick = function() {
+    window.location.assign("https://empirez-2023-edition.pages.dev")
+};
+
+Pinguin_Link2.onclick = function() {
+    window.location.assign("PinguinStudioz.html")
+};
+
+Lunar_Link2.onclick = function() {
     window.location.assign("https://www.roblox.com/groups/33652903/Lunar-Works#!/about")
 };
